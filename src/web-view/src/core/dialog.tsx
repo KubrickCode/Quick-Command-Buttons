9bca6b7a832e67c17332aa32e6bca4138692afe7
kubrickcode <kubrickcode@gmail.com>
2025-09-15 02:00:56 +0000

    Change core index export pattern

--- src/web-view/src/core/dialog.tsx
+++ src/web-view/src/core/dialog.tsx
@@ -3,21 +3,59 @@ import {
   Dialog as ShadcnDialog,
   DialogContent as ShadcnDialogContent,
   DialogHeader as ShadcnDialogHeader,
-  DialogTitle as ShadcnDialogTitle
+  DialogTitle as ShadcnDialogTitle,
+  DialogDescription as ShadcnDialogDescription,
+  DialogFooter as ShadcnDialogFooter,
+  DialogTrigger as ShadcnDialogTrigger,
 } from "./shadcn/dialog";
 
-export const Dialog = ({ children, ...props }: ComponentProps<typeof ShadcnDialog>) => {
+export const Dialog = ({
+  children,
+  ...props
+}: ComponentProps<typeof ShadcnDialog>) => {
   return <ShadcnDialog {...props}>{children}</ShadcnDialog>;
 };
 
-export const DialogContent = ({ children, ...props }: ComponentProps<typeof ShadcnDialogContent>) => {
+export const DialogContent = ({
+  children,
+  ...props
+}: ComponentProps<typeof ShadcnDialogContent>) => {
   return <ShadcnDialogContent {...props}>{children}</ShadcnDialogContent>;
 };
 
-export const DialogHeader = ({ children, ...props }: ComponentProps<typeof ShadcnDialogHeader>) => {
+export const DialogHeader = ({
+  children,
+  ...props
+}: ComponentProps<typeof ShadcnDialogHeader>) => {
   return <ShadcnDialogHeader {...props}>{children}</ShadcnDialogHeader>;
 };
 
-export const DialogTitle = ({ children, ...props }: ComponentProps<typeof ShadcnDialogTitle>) => {
+export const DialogTitle = ({
+  children,
+  ...props
+}: ComponentProps<typeof ShadcnDialogTitle>) => {
   return <ShadcnDialogTitle {...props}>{children}</ShadcnDialogTitle>;
+};
+
+export const DialogDescription = ({
+  children,
+  ...props
+}: ComponentProps<typeof ShadcnDialogDescription>) => {
+  return (
+    <ShadcnDialogDescription {...props}>{children}</ShadcnDialogDescription>
+  );
+};
+
+export const DialogFooter = ({
+  children,
+  ...props
+}: ComponentProps<typeof ShadcnDialogFooter>) => {
+  return <ShadcnDialogFooter {...props}>{children}</ShadcnDialogFooter>;
+};
+
+export const DialogTrigger = ({
+  children,
+  ...props
+}: ComponentProps<typeof ShadcnDialogTrigger>) => {
+  return <ShadcnDialogTrigger {...props}>{children}</ShadcnDialogTrigger>;
 };
--- src/web-view/src/core/index.tsx
+++ src/web-view/src/core/index.tsx
@@ -1,28 +1,10 @@
-export { Badge } from "./badge";
-export { Button } from "./button";
-export {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./card";
-export { Checkbox } from "./checkbox";
-export { Dialog, DialogContent, DialogHeader, DialogTitle } from "./dialog";
-export {
-  DialogClose,
-  DialogDescription,
-  DialogFooter,
-  DialogTrigger,
-} from "./shadcn/dialog";
-export { Input } from "./input";
-export { Label } from "./label";
-export { RadioGroup, RadioGroupItem } from "./radio-group";
-export { Textarea } from "./textarea";
-export {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "./tooltip";
+export * from "./badge";
+export * from "./button";
+export * from "./card";
+export * from "./checkbox";
+export * from "./dialog";
+export * from "./input";
+export * from "./label";
+export * from "./radio-group";
+export * from "./textarea";
+export * from "./tooltip";
