bda0a213d5ff83f1274226e558c2ccea29906878
kubrickcode <kubrickcode@gmail.com>
2025-11-13 12:15:06 +0000

    Setting global environment variables
    Modified to allow all projects to use local environment variables consistently.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,7 +5,3 @@ npm install -g prettier
 npm install -g baedal
 npm install -g @vscode/vsce
 npm install -g ovsx
-
-if [ -f /workspaces/quick-command-buttons/.env ]; then
-  grep -v '^#' /workspaces/quick-command-buttons/.env | sed 's/^/export /' >> ~/.bashrc
-fi
